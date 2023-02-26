95d11cbd04b4248ab3300ecb4c1b1f33ca5316a2
RichrdZam <richardzambranodi@hotmail.com>
2023-02-25 20:30:25 -0500

    Cambio extenso, agregamos un about y un contact al contenido de la pagina

--- Index.js
+++ Index.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 /**Funciones para la navegacion un slide*/
   $('.sidenav').sidenav();
 
-
  /**Funciones del bton para cambiar de modo oscuro y modo claro*/
   $('.dark-mode-toggle').click(function() {
     $('body').toggleClass('dark-mode');
@@ -14,4 +13,23 @@ $(document).ready(function(){
       $(this).html('<i class="material-icons">brightness_6</i>');
     }
   });
+
+// Agrega un desplazamiento suave al hacer clic en los enlaces del navbar
+$('a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    // Almacena el hash
+    var hash = this.hash;
+
+    // Desplaza suavemente hacia la sección correspondiente
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function() {
+      // Agrega el hash (#) a la URL cuando se completa la animación de desplazamiento
+      window.location.hash = hash;
+    });
+  }
+});
+  
 });
